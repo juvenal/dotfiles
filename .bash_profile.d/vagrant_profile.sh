32dde9249455ed11015a4e694ab14332517f3220
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2017-03-13 18:54:35 -0300

    Fix paths
    This patch fixes the pathnames of all virtual machine software installed to the
    new place ${HOME}/virtualMachines, as well as the new brew global install prefix
    /usr/local.

--- .bash_profile.d/vagrant_profile.sh
+++ .bash_profile.d/vagrant_profile.sh
@@ -3,5 +3,5 @@
 # Set some required global path constants for VAGRANT
 #
 
-export VAGRANT_HOME="${HOME}/Documents/.Virtual_Machines/Vagrant/Config"
-export VAGRANT_DOTFILE_PATH="${HOME}/Documents/.Virtual_Machines/Vagrant/Running"
+export VAGRANT_HOME="${HOME}/virtualMachines/Vagrant/Config"
+export VAGRANT_DOTFILE_PATH="${HOME}/virtualMachines/Vagrant/Running"
