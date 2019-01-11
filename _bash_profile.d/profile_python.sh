d45073e412f9416ebaa4cbe0f3826a5eeff6f7d9
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2019-01-11 11:20:20 -0200

    Fix setup of python virtualenvs
    This change fixes an issue with the profile_python.sh were it wasn't
    checking the availability of virtualenvs prior to configure it.

--- _bash_profile.d/profile_python.sh
+++ _bash_profile.d/profile_python.sh
@@ -1,20 +1,37 @@
 # shellcheck disable=SC2148
 
 # Python virtual environments and settings configuration
-#
-
-# Python wirtualenv disable prompt change
-export VIRTUAL_ENV_DISABLE_PROMPT=yes
-
-# Python virtualenvwapper settings
-export WORKON_HOME=${HOME}/.python_virtual_envs
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-	source /usr/local/bin/virtualenvwrapper.sh
-fi
 
 # Python pip should only run if there is a virtualenv currently activated
 export PIP_REQUIRE_VIRTUALENV=true
 
-# cache python pip-installed packages to avoid re-downloading
-export PIP_DOWNLOAD_CACHE=${HOME}/.pip/cache
+# If Python virtual environments were installed
+if [[ -f /usr/local/bin/virtualenvwrapper.sh ]]; then
+    # Activate Python virtualenv commands
+    source /usr/local/bin/virtualenvwrapper.sh
 
+    # Python virtualenvwapper settings
+    export WORKON_HOME="${HOME}/.python_virtual_envs"
+    [[ -d "${WORKON_HOME}" ]] \
+        && chmod 0755 "${WORKON_HOME}" \
+        || mkdir -p -m 0755 "${WORKON_HOME}"
+
+    # Python pip should only run if there is a virtualenv currently activated
+    export PIP_REQUIRE_VIRTUALENV=true
+
+    # cache python pip-installed packages to avoid re-downloading
+    export PIP_DOWNLOAD_CACHE="${HOME}/.pip/cache"
+    [[ -d "${PIP_DOWNLOAD_CACHE}" ]] \
+        && chmod 0755 "${PIP_DOWNLOAD_CACHE}" \
+        || mkdir -p -m 0755 "${PIP_DOWNLOAD_CACHE}"
+else
+    echo "Python virtualenv isn't installed!"
+    echo ""
+    echo "To install it, make sure you are using Homebrew python, then install virtualenv"
+    echo "with the following command:"
+    echo "  $ PIP_REQUIRE_VIRTUALENV=false pip install virtualenvwrapper"
+    echo ""
+    echo "Then, you'll need to reload this config script with:"
+    echo "  $ source ~/.bash_profile.d/profile_python.sh"
+    echo ""
+fi
